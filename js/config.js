7e527aac599792e473036e7726b1ed7b009be9a3
HitHubPocopan <pocopanjugueteria@gmail.com>
2026-05-27 14:45:46 -0300

    14 45

--- js/config.js
+++ js/config.js
@@ -14,6 +14,6 @@
 
 window.App        = window.App || {};
 App.Config = {
-  SUPABASE_URL: 'https://TU_PROYECTO.supabase.co',
-  SUPABASE_KEY: 'TU_ANON_KEY'
+  SUPABASE_URL: 'https://bvkbkgmkkwscextfgeha.supabase.co',
+  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2a2JrZ21ra3dzY2V4dGZnZWhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4OTI1NTMsImV4cCI6MjA5NTQ2ODU1M30.JU2BenKXgtE6b68uKS4-wbbFknwj9Ph3fFAYDxL9_rU'
 };
